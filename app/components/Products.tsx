f76dbf415bbedb1060a8bc115c82291d2104869f
DavidKal29 <davidkalmuk65@gmail.com>
2025-11-06 16:21:25 +0100

    Agregamos un menu con las distintas categorias, pudiendo cambiar la categoria, y se nos aparecerán los productos de esa categoria

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -1,29 +1,24 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import "swiper/css";
 import { Product } from "../types/product";
 
-interface NewProductsProps {
-  products: Product[];
+interface ProductsProps {
+  products: Product[],
+  category: string
 }
 
-export default function NewProducts({products}:NewProductsProps) {
+export default function Products({products,category}:ProductsProps) {
     
   return (
     <section className="w-full max-w-7xl px-4 sm:px-6 mt-16 relative z-10 my-12">
-          <h2 className="text-[28px] sm:text-3xl md:text-5xl font-extrabold mb-10 text-left bg-gradient-to-r from-black via-red-600 to-red-900 bg-clip-text text-transparent tracking-tight">
-            NUEVOS PRODUCTOS 
+          <h2 className="text-[28px] sm:text-3xl md:text-5xl font-extrabold mb-10 text-left bg-gradient-to-r from-black via-red-600 to-red-900 bg-clip-text text-transparent ">
+            {category.toUpperCase()} 
           </h2>
 
-          <Swiper modules={[Autoplay]} autoplay={{ delay: 1000, disableOnInteraction: false }} loop={true} spaceBetween={20} slidesPerView={1} breakpoints={{ 640: { slidesPerView: 1, spaceBetween: 25 }, 768: { slidesPerView: 2, spaceBetween: 30 }, 1024: { slidesPerView: 4, spaceBetween: 35 } }} className="pb-16">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
             {products.map((product:Product, index:number) => (
-              <SwiperSlide key={index}>
+              <div key={index}>
                 <div className="group relative flex flex-col justify-between items-center text-center rounded-3xl shadow-[0_8px_40px_-10px_rgba(0,0,0,0.15)] bg-white overflow-hidden h-[450px] sm:h-[480px] lg:h-[500px] mx-6 sm:mx-0">
                   
-                  {/* Etiqueta NUEVO */}
-                  <div className="absolute top-3 right-3 bg-red-600 text-white text-[10px] sm:text-[11px] font-bold px-3 py-1 rounded-full z-50">NUEVO</div>
-
                   {/* Imagen */}
                   <div className="flex justify-center items-center w-full p-4 sm:p-6 relative h-[55%]">
                     <img src={`/images/products/${product?.image}`} alt={product?.name} className="object-contain max-h-full w-auto drop-shadow-xl transform group-hover:scale-105 transition-all duration-500" />
@@ -43,9 +38,9 @@ export default function NewProducts({products}:NewProductsProps) {
                     <button className="cursor-pointer mt-4 sm:mt-5 bg-gradient-to-r from-black via-red-700 to-black text-white text-xs sm:text-sm font-semibold py-2 px-5 sm:py-2.5 sm:px-6 rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-all duration-300">Ver más</button>
                   </div>
                 </div>
-              </SwiperSlide>
+              </div>
             ))}
-          </Swiper>
+          </div>
         </section>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import { useEffect, useState } from "react";
 import {Product} from './types/product'
 import NewProducts from "./components/NewProducts";
+import Products from "./components/Products";
 
 export default function Home() {
 
@@ -11,8 +12,16 @@ export default function Home() {
 
     const [products,setProducts] = useState<RequiredProduct[]>([])
 
-    const getNewProducts = ()=>{
-      fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/getNewProducts`)
+    const [category,setCategory] = useState('inicio')
+
+    const categories = ['inicio','moviles','tablets','portatiles','ordenadores']
+
+    const changeCategory = (c:string)=>{
+      setCategory(c)
+    }
+
+    const getProducts = (c:string)=>{
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${c === 'inicio' ? 'getNewProducts' : `getProducts/${c}`}`)
       .then(res=>res.json())
       .then(data=>{
         if (data.products) {
@@ -25,14 +34,19 @@ export default function Home() {
 
 
     useEffect(()=>{
-      getNewProducts()
+      getProducts(category)
     },[])
 
     return (
       <div className="flex flex-col justify-start items-center min-h-screen bg-gray-200 relative overflow-hidden">
         <Header />
+        <div className="bg-white p-4 max-[360px]:gap-2 gap-4 flex justify-start items-center max-[360px]:text-sm text-md md:gap-6 lg:gap-12 lg:px-12 drop-shadow-md overflow-hidden overflow-x-auto w-full">
+          {categories.map(c=>(
+            <button onClick={()=>{changeCategory(c);getProducts(c)}} className={`cursor-pointer font-semibold ${category === c ? 'underline decoration-red-500 decoration-2' : ''}`}>{c[0].toUpperCase() + c.slice(1)}</button>
+          ))}
+        </div>
 
-        <NewProducts products={products}></NewProducts>
+        {category === 'inicio' ? (<><NewProducts products={products}></NewProducts></>) : (<><Products products={products} category={category}></Products></>)}
         
           
 
